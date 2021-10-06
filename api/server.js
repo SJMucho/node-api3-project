4cb0ca3d8e257c63826c3d3c50ae8b72e0585b5d
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-10-05 20:25:57 -0400

    server set up

--- api/server.js
+++ api/server.js
@@ -1,12 +1,20 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const server = express();
+
+const usersRouter = require("./users/users-router");
+
+server.use(helmet());
+server.use(morgan());
+server.use(express.json());
+server.use("/api/users", usersRouter);
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it
+const server = require("./api/server.js");
+
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
